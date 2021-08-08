c2bd68ac88b915fce4cf1dc21c534fa32b284b6f
Kuoping Hsu <kuoping.hsu@gmail.com>
2021-08-08 11:47:10 +0800

    Update clint.v

--- rtl/clint.v
+++ rtl/clint.v
@@ -87,7 +87,7 @@ begin
 end
 
 // MSIP is used to trigger an interrupt. The external interrupt is at D[16],
-// defined by simple-rsicv. This is used for software self-test, it connects
+// defined by srv32 core. This is used for software self-test, it connects
 // ex_irq to the interrupt pin at top level of RTL code.
 always @(posedge clk or negedge resetb)
 begin
